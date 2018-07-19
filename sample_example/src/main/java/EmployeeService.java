9f52e39c70af6eb5198c1df24c867919b77a93a6
Amuru Hareesh <amruthhareesh3007@gmail.com>
2018-07-19 16:24:45 +0530

    uploaded from netbeans IDE

---
+++ sample_example/src/main/java/EmployeeService.java
@@ -0,0 +1,15 @@
+import java.util.List;
+ 
+ 
+public interface EmployeeService {
+     
+    public void addEmployee(Employee employee);
+ 
+    public List<Employee> getAllEmployees();
+ 
+    public void deleteEmployee(Integer employeeId);
+ 
+    public Employee getEmployee(int employeeid);
+ 
+    public Employee updateEmployee(Employee employee);
+}
